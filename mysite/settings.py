17f80d5f6696c58fef904ae8506cfb0055fde30d
Rodrigo Alves <raan@cin.ufpe.br>
2016-01-13 19:28:17 -0300

    static files added

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,4 +127,5 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = (os.path.join(os.path.dirname(__file__), '..', 'static'))
 
