84a70a9f0bd35c0ae6a6f025d03fcd021b4f43da
Rodrigo Alves <raan@cin.ufpe.br>
2016-01-13 19:54:49 -0300

    static test

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-)
+    )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
