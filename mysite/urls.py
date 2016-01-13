30a2e7ac628b386dcb2b592c605e8f9f762de240
RodrigoA <RodrigoA>
2016-01-13 19:54:49 -0300

    static test

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,14 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 	url(r'^myscraper/', include('myscraper.urls')),
 	url(r'^admin/', admin.site.urls),
-]
-
-urlpatterns += patterns('',
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.STATIC_ROOT,
-        }),
-    )
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
